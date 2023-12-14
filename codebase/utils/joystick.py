c25b5919a417b4c3defa0576bd626ded310c093e
Erdogan <yildizcontact@gmail.com>
2023-12-14 17:00:14 +0300

    Basic joystick module added.

---
+++ codebase/utils/joystick.py
@@ -0,0 +1,49 @@
+import time
+import pygame
+
+
+class Joystick:
+
+    def __init__(self, yes_button:int=0, no_button:int=1):
+        # Initialize Pygame and the joystick
+        pygame.init()
+        pygame.display.init()
+        pygame.joystick.init()
+        joystick_count = pygame.joystick.get_count()
+        if joystick_count == 0:
+            print("Error, no joystick is found.")
+        else:
+            # Use joystick #0 and initialize it
+            self.joystick = pygame.joystick.Joystick(0)
+            self.joystick.init()
+
+        self.yes_button = yes_button
+        self.no_button = no_button
+
+    @property
+    def axis_0(self):
+        pygame.event.pump()
+        return self.joystick.get_axis(0)
+
+    @property
+    def activate_injection(self):
+        pygame.event.pump()
+        return self.joystick.get_button(self.yes_button)
+
+    def wait_for_button_press(self, info="Please press any joystick button.", max_wait_time_s=5):
+        st = time.time()
+        print(info)
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.JOYBUTTONDOWN:
+                    print(f"Button {event.button} pressed.")
+                    return event.button
+                if time.time() - st > max_wait_time_s: return None
+
+
+if __name__ == "__main__":
+    joystick = Joystick()
+
+    while True:
+        button = joystick.wait_for_button_press()
+        print(button)
